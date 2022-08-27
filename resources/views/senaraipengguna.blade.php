45f1d8dc1679f7edfdce31da1f7081ef4ee7f8f4
Mohd Shafiq Izwan B Abdullah <shfqizwn@outlook.my>
2022-08-27 19:13:06 +0800

    5s

--- resources/views/senaraipengguna.blade.php
+++ resources/views/senaraipengguna.blade.php
@@ -10,32 +10,9 @@
     </div>
 
 	<div class="d-flex my-xl-auto right-content">
-		{{-- <div class="pe-1 mb-xl-0">
-			<button type="button" class="btn btn-success btn-icon me-2"><i class="icon ion-md-add-circle"></i></button>
-		</div>
-
-		<div class="pe-1 mb-xl-0">
-			<button class="btn btn-primary-gradient btn-block"><i class="icon ion-md-add-circle"></i><span>Primary</span></button>
-		</div> --}}
-
 		<div class="pe-1 mb-xl-0">
 			<button class="btn btn-success btn-with-icon btn-block"><i class="icon ion-md-add-circle"></i> Tambah</button>
 		</div>
-
-		{{-- <div class="mb-xl-0">
-			<div class="btn-group dropdown">
-				<button type="button" class="btn btn-primary">14 Aug 2019</button>
-				<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" id="dropdownMenuDate" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				<span class="sr-only">Toggle Dropdown</span>
-				</button>
-				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate" x-placement="bottom-end">
-					<a class="dropdown-item" href="#">2015</a>
-					<a class="dropdown-item" href="#">2016</a>
-					<a class="dropdown-item" href="#">2017</a>
-					<a class="dropdown-item" href="#">2018</a>
-				</div>
-			</div>
-		</div> --}}
 	</div>
 </div>
 <!-- breadcrumb -->
