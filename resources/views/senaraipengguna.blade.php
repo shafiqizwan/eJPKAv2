a9b779864511bd70ef615f5afad0568f59689956
Shafiq Izwan <shafiqizwan@treasury.gov.my>
2022-09-07 14:38:51 +0800

    5s code

--- resources/views/senaraipengguna.blade.php
+++ resources/views/senaraipengguna.blade.php
@@ -156,22 +156,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
-<!-- Internal Data tables -->
-{{-- <script src="../../assets/plugins/datatable/js/jquery.dataTables.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/datatables.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/dataTables.bootstrap5.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/dataTables.buttons.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/buttons.bootstrap5.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/jszip.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/buttons.html5.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/buttons.print.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/js/buttons.colVis.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/pdfmake/pdfmake.min.js"></script> --}}
-{{-- <script src="../../assets/plugins/datatable/pdfmake/vfs_fonts.js"></script> --}}
-
-<!--Internal  Datatable js -->
-{{-- <script src="../../assets/js/table-data.js"></script> --}}
-
 <script type="text/javascript">
 
 $(function () {
